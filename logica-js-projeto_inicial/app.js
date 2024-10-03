ec416958c132182375701d2c719050b7dca1e917
Gabriela Rabelo <129466534+gabrielarabello@users.noreply.github.com>
2024-10-03 17:24:14 -0300

    atualizado

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,17 +1,16 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto = 8;
+let numeroMaximo= 5000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1 );
 console.log(numeroSecreto);
 let chute ;
 let tentativas = 1;
 
-
-
 //enquanto o chute não for igual ao n.s
 while (chute != numeroSecreto) {
-     chute = prompt('Escolha um número entre 1 e 10');
+     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
 // se o chute for real ao número secreto
      if (chute == numeroSecreto ) {
-          alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas` );
+          break;
      } else {
           if (chute > numeroSecreto) {
                alert(`O número secreto é menor que ${chute}`);
@@ -21,3 +20,7 @@ while (chute != numeroSecreto) {
           tentativas++;
      }
 }
+
+// Operador Ternário
+let palavraTentativa = tentativas > 1 ?  'tentativas' : 'tentativa' ;
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.` );
